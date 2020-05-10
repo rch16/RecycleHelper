1611432baff150cd46393107de849cbaa8663d37
Becca Hallam <46824047+rch16@users.noreply.github.com>
2020-05-10 09:39:26 +0100

    Pytesseract implementation

---
+++ Machine Learning/text_recognition.py
@@ -0,0 +1,31 @@
+import cv2
+import pytesseract
+from PIL import Image
+
+def singleImageText(path):
+    img = cv2.imread(path)
+    text = pytesseract.image_to_string(img)
+    print(text)
+
+def multipleImageText(path, matches):
+    count = 0
+    # iterate through images
+    for i in range(1,63):
+        s = path + str(i) + ".jpg"
+        img = cv2.imread(s)
+        text = pytesseract.image_to_string(img)
+        print(i, " : ", text)
+        if any(word in text for word in matches):
+            count += 1
+    # calculate accuracy
+    accuracy = (count/63)*100
+    print(accuracy,"%")
+
+# - - - - - - - - - - text from one image - - - - - - - - - - 
+image_path = "Datasets/label_dataset/cardboard/0.jpg"
+singleImageText(image_path)
+
+# - - - - - - - - - text from folder of images - - - - - - - - -
+matches = ["cardboard", "CARDBOARD", "card", "CARDBOARD" "paper", "PAPER"]
+folder = "Datasets/label_dataset/cardboard/"
+multipleImageText(folder, matches)
