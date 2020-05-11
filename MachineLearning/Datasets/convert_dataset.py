e2de457c85d7a91e2c7bbc4b8b1a1cabb90d5e4c
Becca Hallam <46824047+rch16@users.noreply.github.com>
2020-05-11 17:51:50 +0100

    Update to convert character recognition dataset to csv

--- MachineLearning/Datasets/convert_dataset.py
+++ MachineLearning/Datasets/convert_dataset.py
@@ -5,10 +5,10 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 
-DIRECTORY = 'Datasets/label_dataset/all'
-FORMAT = '.jpg'
+DIRECTORY = 'label_dataset/all'
+DIRECTORY2 = 'MachineLearning/Datasets/letter_dataset'
 
-def make_labels_csv():
+def make_labels_csv_from_folder():
     # get list of files
     files = [f for f in os.listdir(DIRECTORY)]
     # possible categories
@@ -22,13 +22,29 @@ def make_labels_csv():
                     writer = csv.writer(csv_file)
                     writer.writerow(category)
 
-def make_data_csv():
+def make_labels_csv_from_dir(dir,format):
     # get list of files
-    files = [f for f in os.listdir(DIRECTORY)]
+    fileList = []
+    for root, dirs, files in os.walk(dir, topdown=False):
+        for name in files:
+            if name.endswith(format):
+                fullName = os.path.join(root, name)
+                fileList.append(fullName)
+    # extract numbers from each file name
+    for f in fileList:
+        substring = int(f[56:59])
+        category = np.array(substring - 11).flatten()
+        with open("letter_dataset_l.csv",'a') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(category)
+
+def make_data_csv_from_folder(dir):
+    # get list of files
+    files = [f for f in os.listdir(dir)]
     # extract and add to csv
     for f in files:
         if (f != '.DS_Store'):
-            s = '/Users/beccahallam/Documents/GitHub/FYP/Machine Learning/Datasets/label_dataset/all/' + f
+            s = dir + '/' + f
             img = Image.open(s)
             w,h = img.size
             form = img.format
@@ -36,9 +52,58 @@ def make_data_csv():
             img_grey = img.convert('L')
             val = np.asarray(img_grey.getdata(), dtype=np.int).reshape((img_grey.size[1], img_grey.size[0]))
             val = val.flatten()
-            with open("label_dataset.csv",'a') as csv_file:
+            with open("label_datset.csv",'a') as csv_file:
                 writer = csv.writer(csv_file)
                 writer.writerow(val)
     
-make_data_csv()
-make_labels_csv()
+def make_data_csv_from_dir(dir,format):
+    # get list of files
+    fileList = []
+    for root, dirs, files in os.walk(dir, topdown=False):
+        for name in files:
+            if name.endswith(format):
+                fullName = os.path.join(root, name)
+                fileList.append(fullName)
+    # extract and add to csv
+    for f in fileList:
+        if (f != '.DS_Store'):
+            img = Image.open(f)
+            img_grey = img.convert('L')
+            val = np.asarray(img_grey.getdata(), dtype=np.int).reshape((img_grey.size[1], img_grey.size[0]))
+            val = val.flatten()
+            with open("letter_dataset.csv",'a') as csv_file:
+                writer = csv.writer(csv_file)
+                writer.writerow(val)
+
+def find_longest_row():
+    # get list of files
+    files = [f for f in os.listdir(DIRECTORY)]
+    lengths = []
+    print(files)
+    # extract and add to csv
+    for f in files:
+        if (f != '.DS_Store'):
+            s = 'label_dataset/all/' + f
+            img = Image.open(s)
+            w,h = img.size
+            form = img.format
+            mode = img.mode
+            img_grey = img.convert('L')
+            val = np.asarray(img_grey.getdata(), dtype=np.int).reshape((img_grey.size[1], img_grey.size[0]))
+            val = val.flatten()
+            l = len(val)
+            lengths.append(l)
+    print(max(lengths))
+    # find unique elements
+    unique_lengths = []
+    for l in lengths:
+        if l not in unique_lengths:
+            unique_lengths.append(l)
+    #print(unique_lengths)
+    print(min(lengths))
+
+# make_data_csv_from_folder(DIRECTORY,"label_dataset.csv")
+# make_data_csv_from_dir(DIRECTORY2,'.png')
+# make_labels_csv_from_folder()
+make_labels_csv_from_dir(DIRECTORY2,'.png')
+# find_longest_row()
