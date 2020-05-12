bdc08f404d7cf8f1bf560cac3e72110b535fdd7b
Becca Hallam <46824047+rch16@users.noreply.github.com>
2020-05-12 15:28:17 +0100

    Creating App V2

---
+++ AppDevelopment/Version 2/RecycleHelper/RecycleHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecycleHelper
+//
+//  Created by Becca Hallam on 12/05/2020.
+//  Copyright © 2020 Becca Hallam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
