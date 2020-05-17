6ee4a442cf9bd6e2e7a3ee8408eb119cd76c90b3
Becca Hallam <46824047+rch16@users.noreply.github.com>
2020-05-17 23:02:50 +0100

    Implementing Onboarding

--- AppDevelopment/Version 2/RecycleHelper/RecycleHelper/AppDelegate.swift
+++ AppDevelopment/Version 2/RecycleHelper/RecycleHelper/AppDelegate.swift
@@ -14,61 +14,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var storyboard:UIStoryboard?
-    var hasAlreadyLaunched: Bool!
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // Configure Firebase
+        FirebaseApp.configure() 
+        
         window =  UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
+        storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let launchedBefore = UserDefaults.standard.bool(forKey: "LaunchedBefore")
 
-        let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
-
-        if launchedBefore  {
-            // Not the first visit, so show the home screen
-            storyboard = UIStoryboard(name: "Main", bundle: nil)
-            let rootController = storyboard!.instantiateViewController(withIdentifier: "welcome")
-
-            if let window = self.window {
-                window.rootViewController = rootController
-            }
-
+        if launchedBefore  { // Not the first visit
+            // So show the home screen
+            launchStoryboard(identifier: "welcome")
         }
-        else {
-            // First visit, set "launchedBefore" to true
-            UserDefaults.standard.set(true, forKey: "launchedBefore")
+        else { // First visit
+            // Set "launchedBefore" to true
+            UserDefaults.standard.set(true, forKey: "LaunchedBefore")
             // And show the onboarding
-            storyboard = UIStoryboard(name: "Main", bundle: nil)
-            let rootController = storyboard!.instantiateViewController(withIdentifier: "onboarding")
-
-            if let window = self.window {
-                window.rootViewController = rootController
-            }
+            launchStoryboard(identifier: "onboarding")
+            
         }
 
         return true
-//        // Override point for customization after application launch.
-//        // Configure Firebase
-//        FirebaseApp.configure()
-//        // Variable to track if app has already been launched (for onboarding flow)
-//        hasAlreadyLaunched = UserDefaults.standard.bool(forKey: "hasAlreadyLaunched")
-//        print(hasAlreadyLaunched)
-//        // Check if first launch
-//        if (hasAlreadyLaunched) {
-//            hasAlreadyLaunched = true
-//            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//            let initialVC = storyboard.instantiateViewController(withIdentifier: "onboarding")
-//            self.window?.rootViewController = initialVC
-//            self.window?.makeKeyAndVisible()
-//        } else {
-//            UserDefaults.standard.set(true, forKey: "hasAlreadyLaunched")
-//            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//            let initialVC = storyboard.instantiateViewController(withIdentifier: "welcome")
-//            self.window?.rootViewController = initialVC
-//            self.window?.makeKeyAndVisible()
-//        }
-//
-//        return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -82,8 +54,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-    func setHasAlreadyLaunched(){
-        hasAlreadyLaunched = true
+    
+    func launchStoryboard(identifier: String){
+        let viewController = storyboard!.instantiateViewController(withIdentifier: identifier)
+        let navigationController = UINavigationController.init(rootViewController: viewController)
+        //self.window?.rootViewController = navigationController
+        if let window = self.window {
+            window.rootViewController = navigationController
+            window.makeKeyAndVisible()
+        }
     }
 }
