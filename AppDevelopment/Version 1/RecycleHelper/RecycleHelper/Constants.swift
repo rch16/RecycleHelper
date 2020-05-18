7da2d832a90ab7abfea2200badd46989ac526079
Becca Hallam <46824047+rch16@users.noreply.github.com>
2020-05-18 09:16:40 +0100

    Updating App Icon

---
+++ AppDevelopment/Version 1/RecycleHelper/RecycleHelper/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  RecycleHelper
+//
+//  Created by Becca Hallam on 05/01/2020.
+//  Copyright © 2020 Becca Hallam. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let itemViewSegue = "ShowItemSegue"
+    static let videoDataOutput = "VideoDataOutput"
+    static let visionDomain = "VisionViewController"
+}
