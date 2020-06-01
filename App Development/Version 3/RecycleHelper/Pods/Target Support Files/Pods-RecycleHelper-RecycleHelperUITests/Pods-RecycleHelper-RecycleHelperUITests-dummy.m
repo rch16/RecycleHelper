c4e76fcfbeb1f2cd7e2a7f00ae33d4e346145c64
Becca Hallam <rch16@ic.ac.uk>
2020-06-01 09:56:03 +0100

    Starting development round 3

--- App Development/Version 3/RecycleHelper/Pods/Target Support Files/Pods-RecycleHelper-RecycleHelperUITests/Pods-RecycleHelper-RecycleHelperUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RecycleHelper_RecycleHelperUITests : NSObject
-@end
-@implementation PodsDummy_Pods_RecycleHelper_RecycleHelperUITests
-@end
--- App Development/Version 3/RecycleHelper/Pods/Target Support Files/Pods-RecycleHelperTests/Pods-RecycleHelperTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RecycleHelperTests : NSObject
-@end
-@implementation PodsDummy_Pods_RecycleHelperTests
-@end
