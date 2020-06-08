f4f896a26e42490303a6f14e1fd39c1c6fc3d5a6
Becca Hallam <rch16@ic.ac.uk>
2020-06-08 23:11:41 +0100

    Updated funcitonality

--- App Development/Version 2/RecycleHelper/RecycleHelper/Extensions.swift
+++ App Development/Version 2/RecycleHelper/RecycleHelper/Extensions.swift
@@ -25,6 +25,13 @@ extension MKMapView {
   }
 }
 
+extension UIScrollView {
+    func scrollToTop() {
+        let desiredOffset = CGPoint(x: 0, y: -contentInset.top)
+        setContentOffset(desiredOffset, animated: true)
+   }
+}
+
 extension Dictionary where Key == String {
 
     subscript(caseInsensitive key: Key) -> Value? {
