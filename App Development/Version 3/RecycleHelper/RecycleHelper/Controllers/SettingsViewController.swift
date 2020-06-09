c478e2a6eb1fe6f789944fe1d3652f862bcde4dd
Becca Hallam <rch16@ic.ac.uk>
2020-06-09 18:22:43 +0100

    Track recycling progress

--- App Development/Version 3/RecycleHelper/RecycleHelper/Controllers/SettingsViewController.swift
+++ App Development/Version 3/RecycleHelper/RecycleHelper/Controllers/SettingsViewController.swift
@@ -8,22 +8,60 @@
 
 import Foundation
 import UIKit
+import FirebaseDatabase
 
-class SettingsViewController: UITableViewController {
+class SettingsViewController: UITableViewController, SwitchDelegate {
     
+    @IBAction func cancelGoalSelection(segue: UIStoryboardSegue) {}
+    @IBAction func goalWasSelected(segue: UIStoryboardSegue) {
+        if let sourceVC = segue.source as? GoalViewController {
+            let indexPath = IndexPath(row: 0, section: recyclingInfo)
+            targetJustChanged = true
+            let goal = sourceVC.returnData
+            recyclingTarget = K.goalIntArray[goal ?? 6]
+            UserDefaults.standard.set(recyclingTarget, forKey: K.recycleTarget)
+            tableView.reloadRows(at: [indexPath], with: .none)
+        }
+        
+    }
+    
+    var ref: DatabaseReference = Database.database().reference()
     var userName: String!
+    var lastUpdated: String!
+    var animated: Bool = true
+    var recyclingTarget: Int = 50
+    var targetJustChanged: Bool = false
     
     // Number of sections
-    var sectionNum: Int = 5
+    var sectionNum: Int = 7
     
     // Number of rows in section
-    var rowInSection = [2,1,1,1,2]
+    var rowInSection = [2,2,1,1,1,3,1]
+    // 1. App information - about, and last updated data
+    // 2. User personalisation - name and animation
+    // 3. Recycling target
+    // 4. Show onboarding
+    // 5. Change privacy settings
+    // 6. Clear specific data
+    // 7. Reset app
+    
+    let appInfo = 0
+    let userInfo = 1
+    let recyclingInfo = 2
+    let showOnboarding = 3
+    let changePrivacySettings = 4
+    let clearData = 5
+    let clearApp = 6
     
     // Text Field
     var textChanged: ((String) -> Void)?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Load data
+        loadData()
+        // Load Database data
+        loadDatabaseData()
         // Change navigation bar appearance
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
@@ -31,9 +69,6 @@ class SettingsViewController: UITableViewController {
         // Table appearance
         self.tableView.keyboardDismissMode = .interactive
         self.tableView.backgroundColor = .systemGray5
-        // Load data
-        loadData()
-    
     }
     
     func loadData() {
@@ -41,8 +76,24 @@ class SettingsViewController: UITableViewController {
             return
         }
         userName = personalisation
+        
+        let updated = (UserDefaults.standard.object(forKey: K.lastUpdated) as? String)!
+        lastUpdated = updated
+        
+        if let target = UserDefaults.standard.object(forKey: K.recycleTarget) as? Int {
+            recyclingTarget = target
+        }
+        
     }
 
+    func loadDatabaseData() {
+        self.ref.child("Last Updated").observe(.value, with: { (snapshot) in
+            if let updatedDate = snapshot.value as? String {
+                UserDefaults.standard.set(updatedDate, forKey: K.lastUpdated)
+                self.lastUpdated = updatedDate
+            }
+       })
+    }
     
     
     // MARK: - Table View Data Source
@@ -80,7 +131,7 @@ class SettingsViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Table view cells are reused and should be dequeued using a cell identifier.
-        if indexPath.section == 0 {
+        if indexPath.section == appInfo {
             if indexPath.row == 0 {
                 // About
                 let cell = tableView.dequeueReusableCell(withIdentifier: K.aboutCellIdentifier, for: indexPath as IndexPath)
@@ -88,25 +139,38 @@ class SettingsViewController: UITableViewController {
             } else {
                 // Information last updated on
                 let cell = tableView.dequeueReusableCell(withIdentifier: K.updatedInfoCellIdentifier, for: indexPath as IndexPath)
-                let lastUpdated = (UserDefaults.standard.object(forKey: K.lastUpdated) as? String)!
                 
                 cell.detailTextLabel?.text = lastUpdated
-                print(lastUpdated)
                 
                 return cell
             }
             
-        } else if indexPath.section == 1 {
-            // Edit personalisation
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: K.settingsCellIdentifier, for: indexPath as IndexPath) as? SettingsTableViewCell  else {
-                fatalError("The dequeued cell is not an instance of SettingsTableViewCell.")}
-            
-            cell.nameTextField.delegate = self
-            cell.nameTextField.text = userName
-            
-            return cell
-            
-        } else if indexPath.section == 2 {
+        } else if indexPath.section == userInfo {
+            if indexPath.row == 0 {
+                // Edit personalisation
+               guard let cell = tableView.dequeueReusableCell(withIdentifier: K.settingsCellIdentifier, for: indexPath as IndexPath) as? SettingsTableViewCell  else {
+                   fatalError("The dequeued cell is not an instance of SettingsTableViewCell.")}
+               
+                cell.nameTextField.delegate = self
+                cell.nameTextField.text = userName
+                cell.nameTextField.tag = 0
+               
+               return cell
+            } else {
+                // Animated typing on/off
+                guard let switchCell = tableView.dequeueReusableCell(withIdentifier: K.repeatsCellIdentifier, for: indexPath as IndexPath) as? SetsRepeatTableViewCell  else {
+                     fatalError("The dequeued cell is not an instance of SetsRepeatTableViewCell.")}
+                
+                if let animate = UserDefaults.standard.object(forKey: K.animatedTyping) as? Bool  {
+                    animated = animate
+                }
+                
+                switchCell.configure(value: animated)
+                switchCell.delegate = self
+                return switchCell
+            }
+
+        } else if indexPath.section == showOnboarding {
             // Show onboarding
             guard let cell = tableView.dequeueReusableCell(withIdentifier: K.buttonCellIdentifier, for: indexPath as IndexPath) as? ButtonTableViewCell  else {
             fatalError("The dequeued cell is not an instance of ButtonTableViewCell.")}
@@ -116,7 +180,7 @@ class SettingsViewController: UITableViewController {
             cell.delegate = self
             
             return cell
-        } else if indexPath.section == 3 {
+        } else if indexPath.section == changePrivacySettings {
             // Change privacy settings
             guard let cell = tableView.dequeueReusableCell(withIdentifier: K.buttonCellIdentifier, for: indexPath as IndexPath) as? ButtonTableViewCell  else {
             fatalError("The dequeued cell is not an instance of ButtonTableViewCell.")}
@@ -127,12 +191,16 @@ class SettingsViewController: UITableViewController {
             
             return cell
             
-        } else {
+        } else if indexPath.section == clearData {
             // Clear...
             guard let cell = tableView.dequeueReusableCell(withIdentifier: K.buttonCellIdentifier, for: indexPath as IndexPath) as? ButtonTableViewCell  else {
             fatalError("The dequeued cell is not an instance of ButtonTableViewCell.")}
             
             if indexPath.row == 0 {
+                // ... Recycle progress
+                cell.actionBtn.setTitle(K.clearProgressBtnTitle, for: .normal)
+                cell.actionBtn.setTitleColor(.red, for: .normal)
+            } else if indexPath.row == 1 {
                 // ... Collections
                 cell.actionBtn.setTitle(K.clearCollectionsBtnTitle, for: .normal)
                 cell.actionBtn.setTitleColor(.red, for: .normal)
@@ -144,19 +212,66 @@ class SettingsViewController: UITableViewController {
             
             cell.delegate = self
             return cell
+        } else if indexPath.section == recyclingInfo {
+            guard let goalCell = tableView.dequeueReusableCell(withIdentifier: K.eventCellIdentifier, for: indexPath as IndexPath) as? EventTableViewCell  else {
+                fatalError("The dequeued cell is not an instance of EventTableViewCell.")}
+            
+            goalCell.updateGoal(text: "Recycling Goal", goal: recyclingTarget)
+            
+            let accessory: UITableViewCell.AccessoryType = .disclosureIndicator
+            goalCell.accessoryType = accessory
+            
+            return goalCell
+        } else {
+            // Clearing app
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: K.buttonCellIdentifier, for: indexPath as IndexPath) as? ButtonTableViewCell  else {
+            fatalError("The dequeued cell is not an instance of ButtonTableViewCell.")}
+            cell.actionBtn.setTitle(K.resetAppBtnTitle, for: .normal)
+            cell.actionBtn.setTitleColor(.red, for: .normal)
+            cell.delegate = self
+            return cell
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == recyclingInfo {
+            // Select goal
+            tableView.deselectRow(at: indexPath, animated: true)
+            self.performSegue(withIdentifier: K.selectGoalSegue, sender: indexPath)
         }
     }
 
-    
     override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
-            if indexPath.section == 0, indexPath.row == 0 {
+            if indexPath.section == appInfo, indexPath.row == 0 {
                 tableView.deselectRow(at: indexPath, animated: true)
                 let title: String = "Designed and built by Rebecca Hallam"
                 let message: String = "As part of her final year project for an MEng Degree in Electrical and Electronic Engineering at Imperial College London."
                 let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
                 self.present(alert, animated: true)
+            } else if indexPath.section == recyclingInfo {
+                // Select recycling target
+                tableView.deselectRow(at: indexPath, animated: true)
+                self.performSegue(withIdentifier: K.selectWeekdaySegue, sender: indexPath)
+            }
         }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let selectedPath = sender as? IndexPath? {
+            if let nextVC = segue.destination as? GoalViewController, segue.identifier == K.selectGoalSegue, selectedPath!.section == recyclingInfo {
+                let cell = tableView.cellForRow(at: selectedPath!) as! EventTableViewCell
+
+                nextVC.currentData = cell.valueLabel.text
+            }
+        }
+    }
+        
+    // MARK: - SwitchDelegate Methods
+    
+    func didChangeValue(value: Bool) {
+       
+        animated = value
+        UserDefaults.standard.set(animated, forKey: K.animatedTyping)
     }
     
 
@@ -186,7 +301,6 @@ extension SettingsViewController: CellActionDelegate {
                 optionMenu.addAction(cancelAction)
 
                 self.present(optionMenu, animated: true, completion: nil)
-                
             } else {
                 // Delete actions
                 
@@ -194,24 +308,40 @@ extension SettingsViewController: CellActionDelegate {
                 let optionMenu = UIAlertController(title: nil, message: "Are you sure?", preferredStyle: .actionSheet)
                     
                 let yesAction = UIAlertAction(title: "Yes", style: .destructive, handler: { action in
+                    // Haptic feedback
+                    let feedback = UINotificationFeedbackGenerator()
+                    feedback.notificationOccurred(.success)
                     if buttonTitle == K.clearCollectionsBtnTitle {
-                        // Haptic feedback
-                        let feedback = UINotificationFeedbackGenerator()
-                        feedback.notificationOccurred(.success)
                         // Delete bin collections
                         UserDefaults.standard.set([CollectionItem](), forKey: K.binCollections)
                         // Show completion
                         self.showCompletionAlert(message: "Bin collections cleared successfully.")
-
                     } else if buttonTitle == K.clearFavouritesBtnTitle {
-                        // Haptic feedback
-                        let feedback = UINotificationFeedbackGenerator()
-                        feedback.notificationOccurred(.success)
                         // Delete search favourites
                         UserDefaults.standard.set(false, forKey: K.showFavourites)
                         UserDefaults.standard.set([], forKey: K.saveItemKey)
                         // Show completion
                         self.showCompletionAlert(message: "Search favourites cleared successfully.")
+                    } else if buttonTitle == K.clearProgressBtnTitle {
+                       // Clear recycle count
+                       UserDefaults.standard.set(0, forKey: K.recycleCount)
+                       // Show completion
+                       self.showCompletionAlert(message: "Recycling progress reset successfully.")
+                    } else if buttonTitle == K.resetAppBtnTitle {
+                        // Clear all user defaults
+                        UserDefaults.standard.set([], forKey: K.saveItemKey)
+                        UserDefaults.standard.set(false, forKey: K.showFavourites)
+                        UserDefaults.standard.set([CollectionItem](), forKey: K.binCollections)
+                        UserDefaults.standard.set(false, forKey: K.hasPersonalised)
+                        UserDefaults.standard.set("", forKey: K.personalisation)
+                        UserDefaults.standard.set("", forKey: K.lastUpdated)
+                        UserDefaults.standard.set("", forKey: K.userLocation)
+                        UserDefaults.standard.set(true, forKey: K.animatedTyping)
+                        UserDefaults.standard.set(0, forKey: K.recycleCount)
+                        UserDefaults.standard.set(50, forKey: K.recycleCount)
+                        UserDefaults.standard.set(false, forKey: K.shownInstructions)
+                        // Show completion
+                        self.showCompletionAlertAndSegue(message: "App reset successfully.")
                     }
                 })
                 let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
@@ -225,6 +355,16 @@ extension SettingsViewController: CellActionDelegate {
             
 
         }
+    }
+    
+    // Show completion and then segue
+    func showCompletionAlertAndSegue(message: String) {
+        let alert = UIAlertController(title: "Success!", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { (action) in
+            // Segue to onboarding
+            self.performSegue(withIdentifier: K.showOnboardingSegue, sender: nil)
+        }))
+        self.present(alert, animated: true)
     }
     
     // Show completion
@@ -253,13 +393,17 @@ extension SettingsViewController: UITextFieldDelegate {
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        userName = textField.text
+        if textField.tag == 0 {
+            userName = textField.text
+        }
         textField.resignFirstResponder()
         return true
      }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        userName = textField.text
-        UserDefaults.standard.set(userName, forKey: K.personalisation)
+        if textField.tag == 0 {
+            userName = textField.text
+            UserDefaults.standard.set(userName, forKey: K.personalisation)
+        }
     }
 }
